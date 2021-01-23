3a80486cde95ab523107801c194f8142949d121e
dutarrr <duzguntutar@gmail.com>
2021-01-23 18:08:01 +0300

    image draw

--- core/src/com/dutar/catchfly/Main.java
+++ core/src/com/dutar/catchfly/Main.java
@@ -7,27 +7,34 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture imgBackground;
+    float screenX;
+    float screenY;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        imgBackground = new Texture("background.png");
+        screenX = Gdx.graphics.getWidth();
+        screenY = Gdx.graphics.getHeight();
+
+        System.out.println("x: " + screenX);
+        System.out.println("y: "+ screenY);
+    }
+
+    @Override
+    public void render() {
+//		Gdx.gl.glClearColor(0, 0, 1, 0);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(imgBackground, 0, 0, screenX, screenY);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        imgBackground.dispose();
+    }
 }
