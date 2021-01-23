7d19f8fa049f5da3a838ad3e279eaaae033d323e
dutarrr <duzguntutar@gmail.com>
2021-01-24 00:11:19 +0300

    fly move

--- core/src/com/dutar/catchfly/Main.java
+++ core/src/com/dutar/catchfly/Main.java
@@ -12,23 +12,63 @@ public class Main extends ApplicationAdapter {
     float screenX;
     float screenY;
 
+    Texture imgFly;
+    float imgFlyX;
+    float imgFlyY;
+    float imgFlyWidth;
+    float imgFlyHeight;
+
+    int state = 0;
+    float velocity = 0;
+    float gravity = 0.1f;
+
+
     @Override
     public void create() {
         batch = new SpriteBatch();
-        imgBackground = new Texture("background.png");
+
         screenX = Gdx.graphics.getWidth();
         screenY = Gdx.graphics.getHeight();
 
-        System.out.println("x: " + screenX);
-        System.out.println("y: "+ screenY);
+        imgBackground = new Texture("background.png");
+
+        imgFly = new Texture("fly.png");
+        imgFlyX = screenX / 1.5f;
+        imgFlyY = screenY / 2;
+        imgFlyWidth = screenX / 15;
+        imgFlyHeight = screenY / 10;
     }
 
     @Override
     public void render() {
 //		Gdx.gl.glClearColor(0, 0, 1, 0);
 //		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if (state == 0) { //End
+            if (Gdx.input.justTouched()) {
+                if (state == 0)
+                    state = 1;
+            }
+        } else if (state == 1) { //start
+            if (imgFlyY > 0 && imgFlyY < screenY / 1.1f) {
+                velocity += gravity;
+                imgFlyY -= velocity;
+            } else if (imgFlyY <= 0) {
+                state = 0;
+                imgFlyY = screenY / 2;
+                velocity = 0;
+            } else if (imgFlyY >= screenY / 1.1f) {
+                imgFlyY = screenY / 1.11f;
+                velocity = 0;
+            }
+
+            if (Gdx.input.justTouched())
+                velocity -= 3;
+        }
+
         batch.begin();
         batch.draw(imgBackground, 0, 0, screenX, screenY);
+        batch.draw(imgFly, imgFlyX, imgFlyY, imgFlyWidth, imgFlyHeight);
         batch.end();
     }
 
@@ -36,5 +76,6 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         imgBackground.dispose();
+        imgFly.dispose();
     }
 }
