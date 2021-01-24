5a6fec5f79cf8ab3e716b5b09dbc099d7b26dda6
dutarrr <duzguntutar@gmail.com>
2021-01-24 23:07:31 +0300

    gdx

--- android/src/com/dutar/catchfly/AndroidLauncher.java
+++ android/src/com/dutar/catchfly/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.dutar.catchfly.Main;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
--- core/src/com/dutar/catchfly/Main.java
+++ core/src/com/dutar/catchfly/Main.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class Main extends ApplicationAdapter {
     SpriteBatch batch;
@@ -18,8 +21,8 @@ public class Main extends ApplicationAdapter {
     float imgFlyWidth;
     float imgFlyHeight;
 
-    int state = 0;
-    float velocity = 0;
+    float velocityX = 5;
+    float velocityY = 5;
     float gravity = 0.1f;
 
 
@@ -33,37 +36,30 @@ public class Main extends ApplicationAdapter {
         imgBackground = new Texture("background.png");
 
         imgFly = new Texture("fly.png");
-        imgFlyX = screenX / 1.5f;
+        imgFlyX = screenX / 2;
         imgFlyY = screenY / 2;
-        imgFlyWidth = screenX / 15;
-        imgFlyHeight = screenY / 10;
+        imgFlyWidth = screenX / 16;
+        imgFlyHeight = screenY / 9;
     }
 
     @Override
     public void render() {
-//		Gdx.gl.glClearColor(0, 0, 1, 0);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (state == 0) { //End
-            if (Gdx.input.justTouched()) {
-                if (state == 0)
-                    state = 1;
-            }
-        } else if (state == 1) { //start
-            if (imgFlyY > 0 && imgFlyY < screenY / 1.1f) {
-                velocity += gravity;
-                imgFlyY -= velocity;
-            } else if (imgFlyY <= 0) {
-                state = 0;
-                imgFlyY = screenY / 2;
-                velocity = 0;
-            } else if (imgFlyY >= screenY / 1.1f) {
-                imgFlyY = screenY / 1.11f;
-                velocity = 0;
-            }
+        imgFlyX = imgFlyX + velocityX;
+        imgFlyY = imgFlyY + velocityY;
 
-            if (Gdx.input.justTouched())
-                velocity -= 3;
+        if (imgFlyX <= 0) {
+            velocityX = 5;
+        }
+        if (imgFlyX >= screenX - imgFlyWidth) {
+            velocityX = -5;
+        }
+
+        if (imgFlyY <= 0) {
+            velocityY = 5;
+        }
+        if (imgFlyY >= screenY-imgFlyHeight) {
+            velocityY = -5;
         }
 
         batch.begin();
